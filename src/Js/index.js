c706b7cae9dcc8a7621d619f84eff747e815c4dc
= <shiroe.gaming.dev@gmail.com>
2024-04-07 13:33:19 -0300

    show more e show less button and temporary images to projects

--- src/Js/index.js
+++ src/Js/index.js
@@ -1,17 +1,28 @@
-const buttonShowProjects = document.querySelector('.btn-show-projects');
+//const buttonShowProjects = document.querySelector('.btn-show-projects');
+const buttonShowMoreProjects = document.getElementById("show-more");
+const buttonShowLessProjects = document.getElementById("show-less");
 const inactiveProjects = document.querySelectorAll('.project:not(.active)');
 
-buttonShowProjects.addEventListener('click', () => {
+buttonShowMoreProjects.addEventListener('click', () => {
     ShowMoreProjects();
-    HiddenShowMoreButton();
 });
 
-function HiddenShowMoreButton() {
-    buttonShowProjects.classList.add('hidden');
-}
+buttonShowLessProjects.addEventListener('click', () => {
+    ShowLessProjects();
+})
 
-function ShowMoreProjects() {
+function ShowMoreProjects(){
     inactiveProjects.forEach(inactiveProject => {
         inactiveProject.classList.add('active');
     });
+    buttonShowMoreProjects.classList.add('hidden');
+    buttonShowLessProjects.classList.remove('hidden');
+}
+
+function ShowLessProjects(){
+    inactiveProjects.forEach(inactiveProject => {
+        inactiveProject.classList.remove('active');
+    });
+    buttonShowMoreProjects.classList.remove('hidden');
+    buttonShowLessProjects.classList.add('hidden');
 }
